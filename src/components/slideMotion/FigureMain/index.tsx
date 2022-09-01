692c4eef0f49728c540aff78469bbf295db20f0d
hoseinkhanbeigi <hosein@hoseins-iMac.local>
2022-09-01 11:46:58 +0430

    fixed bugs

--- src/components/slideMotion/FigureMain/index.tsx
+++ src/components/slideMotion/FigureMain/index.tsx
@@ -1,37 +1,43 @@
-import { useState } from "react"
+import { useMemo, useState } from "react";
 import clsx from "clsx";
 import { FigureMainProps } from "../../../types";
 import { useTileFx } from "../../../hooks/useTileFx";
 
-
-const FigureMain: React.FC<FigureMainProps> = ({
-    dataSort,
-    url,
-}) => {
+const FigureMain: React.FC<FigureMainProps> = ({ dataSort, url }) => {
     const root = useTileFx("figure");
     const [isLoaded, setIsLoaded] = useState(true);
     const handleOnLoad = () => {
-        setIsLoaded(false)
-    }
+        setIsLoaded(false);
+    };
+
+    const srcUlr = useMemo(() => {
+        return url.main.url;
+    }, []);
+
     const arr: Array<number> = Array(4);
     return (
         <>
-            <div className="slide__figure slide__figure--main" data-sort={dataSort.main.dataSort}>
+            <div
+                className="slide__figure slide__figure--main"
+                data-sort={dataSort.main.dataSort}
+            >
                 <div className="slide__figure-inner" ref={root.figure}>
                     {[...arr].map((_: number, index: number) => {
-                        return <img
-                            key={index}
-                            className={clsx("slide__figure-img", {
-                                filterImg: isLoaded,
-                            })}
-                            onLoad={handleOnLoad}
-                            src={url.main.url}
-                            alt="A description of the image."
-                            width="690"
-                            height="100%"
-                            loading="lazy"
-                            decoding="async"
-                        ></img>
+                        return (
+                            <img
+                                key={index}
+                                className={clsx("slide__figure-img", {
+                                    filterImg: isLoaded,
+                                })}
+                                onLoad={handleOnLoad}
+                                src={srcUlr}
+                                alt="A description of the image."
+                                width="690"
+                                height="100%"
+                                loading="lazy"
+                                decoding="async"
+                            ></img>
+                        );
                     })}
                 </div>
             </div>
@@ -39,4 +45,4 @@ const FigureMain: React.FC<FigureMainProps> = ({
     );
 };
 
-export default FigureMain
+export default FigureMain;
