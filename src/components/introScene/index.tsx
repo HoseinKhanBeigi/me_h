a77049711bfb9af798c2baf5af8aea4d6468be29
hossein <hossein@hosseins-MacBook-Pro.local>
2024-05-12 12:23:28 +0330

    add skill to introScene

--- src/components/introScene/index.tsx
+++ src/components/introScene/index.tsx
@@ -1,19 +1,21 @@
-import { TextBlock } from "../textBlock"
+import { TextBlock } from "../textBlock";
 
 export const IntroScene = () => {
-    return (
-        <section className="scene-intro" id="intro">
-            <div className="static-container">
-                <h1 className="title">
-                    <span className="iuri">Hossein</span>
-                    <span className="func">.is()</span>
-                </h1>
+  return (
+    <section className="scene-intro" id="intro">
+      <div className="static-container">
+        <h1 className="title">
+          <span className="iuri">Hossein</span>
+          <span className="func">.is()</span>
+        </h1>
 
-                <TextBlock>
-                    <p className="-purple">Hossein Khan Beigi</p>
-                    <p className="-gray">Creative frontend developer</p>
-                </TextBlock>
-            </div>
-        </section>
-    )
-}
+        <TextBlock>
+          <p className="-purple">Hossein Khan Beigi</p>
+          <p className="-gray">
+            Creative frontend - Game - unity - godot - pixijs developer
+          </p>
+        </TextBlock>
+      </div>
+    </section>
+  );
+};
